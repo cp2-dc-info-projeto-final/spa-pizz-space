bd259d238516df3ac0a766f67961fc9091b95828
Rafsneto <rafaoneto@gmail.com>
2024-09-23 16:42:10 -0300

    removendo pasta Codigos antiga

--- Codigos/db/Create.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE usuario(
-    nome text NOT NULL, 
-    email text NOT NULL UNIQUE,
-    data_nasc date NOT NULL,
-    num_cell numeric NOT NULL UNIQUE,
-    senha text NOT NULL
-    );
