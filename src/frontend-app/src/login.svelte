7ca9b5b96ee2c45bbbed57c46749bb0ad1e7c91a
Rafsneto <rafaoneto@gmail.com>
2024-10-22 16:24:17 -0300

    fazendo o login

--- src/frontend-app/src/login.svelte
+++ src/frontend-app/src/login.svelte
@@ -1,48 +1,4 @@
 <script>
-<<<<<<< HEAD
-  import axios from "axios";
-
-  let email = "";
-  let senha = "";
-  let error = null;
-  let usuarios = null;
-  let colunas_usuarios = null;
-  let resultado = null;
-
-  const API_BASE_URL = "http://localhost:3000";
-
-  const axiosInstance = axios.create({
-        withCredentials: true,
-        baseURL: API_BASE_URL,
-        responseType: "json",
-        headers: {
-                Accept: "application/json",
-            }
-    });
-    
-    const loginUsuario = async () => {
-      try {
-        let res = await axiosInstance.post("/login",
-          {
-            email,
-            senha
-          }
-        );
-
-        resultado = res.data;
-
-        // Redirecionar para uma página protegida após login bem-sucedido
-        if (resultado && resultado.status === "success") { 
-            window.location.href = "/index.html";  
-        }
-        error = null; // Limpa o erro se a requisição for bem-sucedida
-      } catch (err) {
-        error = err.response?.data?.message || err.message;
-        resultado = null; // Limpa o resultado em caso de erro
-      }
-      
-    };
-=======
   // import { onMount } from 'svelte';
   import axios from "axios";
       let email = "";
@@ -85,28 +41,11 @@
         
       };
 
-
->>>>>>> a2beb24 (fazendo código de login no frontend e no backend)
 </script>
 
 <main>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<<<<<<< HEAD
-    <div class="login-container">
-        <h2>Login</h2>
-        <form on:submit={handleSubmit}>
-            <div>
-            <label for="email">E-mail:</label>
-            <input type="email" id="email" bind:value={email} required/>
-            </div>
-            <div> 
-            <label for="password">Senha:</label>
-            <input type="password" id="password" bind:value={senha} required/>
-            </div>
-            <button type="submit">Entrar</button>
-    </div>
-=======
     <div class="card">
       <form on:submit|preventDefault={loginUsuario}>
         <div>
@@ -140,6 +79,4 @@
         </div>
       </form>
   </div>
-
->>>>>>> a2beb24 (fazendo código de login no frontend e no backend)
 </main>
